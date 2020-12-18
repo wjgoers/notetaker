4c78afa691426a6a0e3965ad939070a67523ff7f
Bill Goers <wjgoers@gmail.com>
2020-12-18 13:00:39 -0600

    heroku prep

---
+++ routes/apiRoutes.js
@@ -0,0 +1,69 @@
+const randomNum = require("uuid/v1")
+
+module.exports = function(app){
+  let notes = require("../db/db.json")
+
+  app.get("/api/notes", (req, res)=>{
+    return res.json(notes)
+    console.log('get route', notes)
+  })
+  
+  // app.get("/api/notes/:id", (req, res) => {
+  //   // console.log(req.params.id)
+  //   const id = req.params.id;
+  //   let found;
+  //   notes.forEach(n => {
+  //     if (id == n.id){
+  //       found = n;
+  //       return res.json(n)
+  //     }
+  //   })
+  //   return res.json(false)
+  // })
+
+
+  app.post("/api/notes", (req, res) => {
+    const newNote = {
+        id: randomNum(),
+        title: req.body.title, 
+        text: req.body.text,      
+    }
+
+    // if (notes.length === 0){
+    //   newNote.id = 1
+    // } else {
+    //   newNote.id = (notes[notes.length-1].id + 1);
+    // }
+    notes.push(newNote);
+    let jsonNotes = JSON.stringify(notes)
+    console.log(notes)
+    // fs.writeFile("./db/db.json", jsonNotes, function(err) {
+    //   if (err) {
+    //     return console.log(err);
+    //   }
+    //   console.log("Success!");
+    // })
+    res.json(notes)
+  })
+
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    let found;
+    notes.splice(id, 1) 
+    console.log(notes)
+    //   if(id == n.id){
+    //     notes.splice(index,1)
+    //     const notesCopy = notes.slice();
+    //     let jsonNotes = JSON.stringify(notesCopy)
+    //     // fs.writeFile("./db/db.json", jsonNotes, function(err) {
+    //     //   if (err) {
+    //     //     return console.log(err);
+    //     //   }
+    //     //   console.log("Success!");
+    //     // })
+
+    //   }
+    // })
+    res.json(notes);
+})
+}
